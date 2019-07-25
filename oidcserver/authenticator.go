2f875bc9af260fa6d20481f276bb8dcaee59d22b
Andy Lindeman <alindeman@salesforce.com>
2019-07-25 13:36:38 -0400

    Support connectors with HTTP handlers
    This is required for connectors that handle their own callbacks or have
    other needs to serve HTTP.
    
    Serving HTTP introduced yet another bit of configuration that will need
    to be passed from the server to the connector: the URL and/or path where
    the `Connector`'s `http.Handler` will be mounted. In our case we've
    chosen `/connector/:id`, but it's obviously not something we want
    connectors hard-coding. Connectors may also not have an awareness of
    their own ID. It's also possible that Deci itself is mounted in a path
    (e.g., `/deci`), so the full path may end up being something like
    `/deci/connector/:id`. Most connectors will need to know this URL to
    properly generate links or HTTP redirects in its own `http.Handler`.
    
    My first tactic to support additional configuration options was to
    create a higher level `ConnectorBuilder` function that was passed a set
    of `ConnectorOptions` and returned `(Connector, error`). Something like:
    
    ```go
    type ConnectorBuilder func(opts ConnectorOpts) (Connector, error)
    
    connectorBulders := map[string]ConnectorBuilder{
      "static": func(opts ConnectorOpts) (Connector, error) {
        // opts.Authenticator would make the Authenticator available
        // opts.BaseURL would make the BaseURL available
        //
        // Additional fields could be added to opts in a
        // backwards-compatible way later
    
        // ...
        return connector, nil
       },
    }
    
    server, err := NewServer(..., connectorBuilders, ...)
    ```
    
    While I think this would solve the problem, it adds significant
    complexity to the constructor creation process. It also makes connectors
    more awkward to test on their own because a `ConnectorBuilder` must be
    used (which looks awkward in a test that otherwise doesn't care how the
    `Connector` is built, because it's being tested independently of
    `Server`) or the `ConnectorBuilder` is left untested, which leaves
    important code untested.
    
    I settled instead on constructing a `Server` first, then adding
    `Connector` instances later. The `Server`'s public API becomes
    usable when building the `Connector`, but smaller interfaces (e.g.,
    `Authenticator` or a `string` from the return value of `ConnectorURL`)
    can be passed as constructor arguments or set directly on `Connector`
    structs. This makes it more pleasant to test a `Connector` on its own
    (without `Server`) and avoids additional types like `ConnectorBuilder`
    and such that look awkward in Go code.
    
    I've also ripped out the awareness of `URL.Path` in `Server`'s mux. This
    follows the convention of other Go `http.Handler` (e.g., `FileServer`)
    which always serve relative to their own view of `/`, but can be mounted
    at any path as long as `http.StripPrefix` is used. This reduces the need to
    test non-`/` paths and allows more flexiblility in mounting the `Server`
    in `main`.

--- oidcserver/authenticator.go
+++ oidcserver/authenticator.go
@@ -3,24 +3,21 @@ package oidcserver
 import (
 	"context"
 	"fmt"
-	"path"
 
 	storagepb "github.com/heroku/deci/proto/deci/storage/v1beta1"
 )
 
-// authenticator is passed to connectors to allow them to mark users as
-// authenticated, and provide their information. It's basically a thin
-// wrapper for the main Server type, to avoid exposing the Authenticate
-// method on server's public type
-type authenticator struct {
-	s *Server
+// Authenticator is capable of associating the user's identity with a given
+// authID, then returning the final redirect URL.
+//
+// Server implements Authenticator.
+type Authenticator interface {
+	// Authenticate associates the user's identity with the given authID, then
+	// returns final redirect URL.
+	Authenticate(ctx context.Context, authID string, ident Identity) (returnURL string, err error)
 }
 
-// finalizeLogin associates the user's identity with the current AuthRequest, then returns
-// the approval page's path.
-// TODO(lstoll) - this becomes the authorizer methd
-func (a *authenticator) Authenticate(ctx context.Context, authID string, ident Identity) (returnURL string, err error) {
-	// func (s *Server) finalizeLogin(ctx context.Context, identity Identity, authReq *storagepb.AuthRequest, authReqVers string) (string, error) {
+func (s *Server) Authenticate(ctx context.Context, authID string, ident Identity) (returnURL string, err error) {
 	claims := &storagepb.Claims{
 		UserId:        ident.UserID,
 		Username:      ident.Username,
@@ -30,7 +27,7 @@ func (a *authenticator) Authenticate(ctx context.Context, authID string, ident I
 	}
 
 	authReq := &storagepb.AuthRequest{}
-	authReqVers, err := a.s.storage.Get(ctx, authReqKeyspace, authID, authReq)
+	authReqVers, err := s.storage.Get(ctx, authReqKeyspace, authID, authReq)
 	if err != nil {
 		return "", err
 	}
@@ -39,7 +36,7 @@ func (a *authenticator) Authenticate(ctx context.Context, authID string, ident I
 	authReq.Claims = claims
 	authReq.ConnectorData = ident.ConnectorData
 
-	if _, err := a.s.storage.Put(ctx, authReqKeyspace, authReq.Id, authReqVers, authReq); err != nil {
+	if _, err := s.storage.Put(ctx, authReqKeyspace, authReq.Id, authReqVers, authReq); err != nil {
 		return "", fmt.Errorf("failed to update auth request: %v", err)
 	}
 
@@ -48,8 +45,8 @@ func (a *authenticator) Authenticate(ctx context.Context, authID string, ident I
 		email = email + " (unverified)"
 	}
 
-	a.s.logger.Infof("login successful: connector %q, username=%q, email=%q, groups=%q",
+	s.logger.Infof("login successful: connector %q, username=%q, email=%q, groups=%q",
 		authReq.ConnectorId, claims.Username, email, claims.Groups)
 
-	return path.Join(a.s.issuerURL.Path, "/approval") + "?req=" + authReq.Id, nil
+	return s.absURL("/approval") + "?req=" + authReq.Id, nil
 }
