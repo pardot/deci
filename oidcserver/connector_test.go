eb842be61bf6694a8991856848caccab011d9f37
Lincoln Stoll <lincoln.stoll@salesforce.com>
2019-09-27 15:02:37 -0500

    Implement acr_values and amr handling
    This implements as per proposal in #35
    
    the acr_values field is handled if requested, and passed through to the
    connector. The connector can then choose to act or ignore these values.
    
    The LoginRequest returned from the connector has a field for AMR and ACR. If
    these are set, the values will be embedded in the returned token.

--- oidcserver/connector_test.go
+++ oidcserver/connector_test.go
@@ -8,7 +8,7 @@ import (
 
 // newMockConnector returns a mock connector which requires no user interaction. It always returns
 // the same (fake) identity.
-func newMockConnector(authenticator Authenticator) Connector {
+func newMockConnector(authenticator Authenticator) *mockConnector {
 	return &mockConnector{
 		Identity: Identity{
 			UserID:        "0-385-28089-0",
@@ -31,12 +31,21 @@ type mockConnector struct {
 	// The returned identity.
 	Identity Identity
 
+	// preAuthenticate will be called if non-nil on each LoginPage with the
+	// passed loginrequest, and will return the identity it returns rather than
+	// the static value.
+	preAuthenticate func(lr LoginRequest) Identity
+
 	authenticator Authenticator
 }
 
 func (m *mockConnector) LoginPage(w http.ResponseWriter, r *http.Request, lr LoginRequest) {
 	// just auto mark the session as good, and redirect the user to the final page
-	ret, err := m.authenticator.Authenticate(r.Context(), lr.AuthID, m.Identity)
+	ident := m.Identity
+	if m.preAuthenticate != nil {
+		ident = m.preAuthenticate(lr)
+	}
+	ret, err := m.authenticator.Authenticate(r.Context(), lr.AuthID, ident)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Internal error: %v", err), http.StatusInternalServerError)
 		return
